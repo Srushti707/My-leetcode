95c5b7612280a1b51651c6b9514bf375b550ff55
Srushti Jamghare <srushtij707@gmail.com>
2026-03-06 00:07:34 +0530

    Add solution for Single Number

---
+++ solutions/easy/Single Number.java
@@ -0,0 +1,15 @@
+// Title: Single Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int num:nums)
+        {
+            result^=num;
+        }
+        return result;
+    }
+}
