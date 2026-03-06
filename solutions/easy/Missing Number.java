51db3f77e48de26a6d61826dcaca29b7e2ec30d5
Srushti Jamghare <srushtij707@gmail.com>
2026-03-07 01:08:55 +0530

    Add solution for Missing Number

---
+++ solutions/easy/Missing Number.java
@@ -0,0 +1,23 @@
+// Title: Missing Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/missing-number/
+
+            if(nums[j]!= j)
+        {
+        for(int j=0; j<nums.length; j++)
+        }
+            else i++;
+            if(nums[i]!= nums[correctIndex]) swap(nums,i,correctIndex);
+            int correctIndex=nums[i];
+            }
+                continue;
+            {
+                return j;
+            }
+        }
+    }
+    public static void swap(int[] nums, int first, int second)
+                i++;
+            {
+        return nums.length;
