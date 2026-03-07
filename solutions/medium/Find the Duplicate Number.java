6df16a5811fa2ffcf6c0feed8216eca79cb88e9a
Srushti Jamghare <srushtij707@gmail.com>
2026-03-07 20:03:31 +0530

    Add solution for Find the Duplicate Number

---
+++ solutions/medium/Find the Duplicate Number.java
@@ -0,0 +1,16 @@
+// Title: Find the Duplicate Number
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-duplicate-number/
+
+        {
+            if(arr.contains(num)) return num;
+            else arr.add(num);
+        }
+        return -1;
+    }
+}
+        for(int num: nums)
+        Set<Integer> arr= new HashSet<>();
+    public int findDuplicate(int[] nums) {
+class Solution {
