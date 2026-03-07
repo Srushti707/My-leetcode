064cea1960988057e5f7c5974da7924bf4d5ea8e
Srushti Jamghare <srushtij707@gmail.com>
2026-03-07 17:23:15 +0530

    Add solution for Find All Numbers Disappeared in an Array

---
+++ solutions/easy/Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,23 @@
+// Title: Find All Numbers Disappeared in an Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+        }
+            System.out.println(j+1);
+            System.out.println(nums[j]);
+        {
+        for(int j=0; j<nums.length; j++)
+        List<Integer> arr= new ArrayList<>();
+        System.out.println(Arrays.toString(nums));
+        }
+            else i++;
+            if(nums[i]!=nums[correctIndex]) swap(nums,i,correctIndex);
+            int correctIndex=nums[i]-1;
+        {
+            if(nums[j]!=j+1 ) arr.add(j+1);
+        return arr;
+    }
+    public static void swap(int[] nums, int first,int second)
+        while(i<nums.length)
+        int i=0;
