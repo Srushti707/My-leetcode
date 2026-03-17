c5a93237696380b46edc1994b4a6dbc8d60b0e0b
Srushti Jamghare <srushtij707@gmail.com>
2026-03-17 10:06:55 +0530

    Add solution for Roman to Integer

--- solutions/easy/Roman to Integer.java
+++ solutions/easy/Roman to Integer.java
@@ -3,17 +3,21 @@
             // Language: Java
             // Link: https://leetcode.com/problems/roman-to-integer/
 
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
         map.put('M',1000);
+        int sum=0;
         for(int i=0; i<s.length(); i++)
         {
             if(i+1<s.length() &&map.get(s.charAt(i+1))> map.get(s.charAt(i)))
-        int sum=0;
+            {
                 sum-=map.get(s.charAt(i));
             }
-        }
             else sum+=map.get(s.charAt(i));
+        }
         return sum;
-    }
-            System.out.println(map.get(s.charAt(i)));
-            {
+    } 
 }
