c5c955ad6326f8691a04a44bf67eb07f2b8e112c
Srushti Jamghare <srushtij707@gmail.com>
2026-03-22 20:56:44 +0530

    Add solution for Happy Number

---
+++ solutions/easy/Happy Number.java
@@ -0,0 +1,22 @@
+// Title: Happy Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/happy-number/
+
+                // int digit=n%10;
+            }
+                n=n/10;
+                sum+=((n%10)*(n%10));
+        if(sum==1 || sum==7) return true;
+        sum=0;
+        n=sum;
+                System.out.println(sum);
+        }
+        if(n==7 || n==1) return true;
+        return false;
+    }
+            {
+            while(n!=0)
+        {
+        while (n>=10)
+}
