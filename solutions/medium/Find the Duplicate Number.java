67f0b0aad0bdfeb2e6614c4b7151e7b630cf005a
Srushti Jamghare <srushtij707@gmail.com>
2026-03-26 21:35:16 +0530

    Add solution for Find the Duplicate Number

--- solutions/medium/Find the Duplicate Number.java
+++ solutions/medium/Find the Duplicate Number.java
@@ -3,20 +3,20 @@
             // Language: Java
             // Link: https://leetcode.com/problems/find-the-duplicate-number/
 
-            if(nums[i]!=nums[correctIndex]) swap(nums,i,correctIndex);
-        }
-    }
-    public void swap(int[] nums, int first, int second)
-    {
-            else i++;
         for(int j=0; j<nums.length; j++)
         {
             if(j!= nums[j]-1) return nums[j];
         }
+        return -1;
+    }
+    public void swap(int[] nums, int first, int second)
+    {
+        }
+            else i++;
+            if(nums[i]!=nums[correctIndex]) swap(nums,i,correctIndex);
             int correctIndex=nums[i]-1;
         {
         while(i<nums.length)
         int i=0;
     public int findDuplicate(int[] nums) {
 class Solution {
-        return -1;
