4693d9d9ae334a560518ce8752400f1603cf2445
Srushti Jamghare <srushtij707@gmail.com>
2026-03-27 16:41:30 +0530

    Add solution for Restore Finishing Order

---
+++ solutions/easy/Restore Finishing Order.java
@@ -0,0 +1,22 @@
+// Title: Restore Finishing Order
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/restore-finishing-order/
+
+        for(int o:order)
+        {
+            if(friends2.contains(o))
+        }
+            friends2.add(friend);
+        }
+        int i=0;
+            {
+                ans[i]=o;
+            }
+                i++;
+        {
+        for(int friend:friends)
+        int[] ans= new int[friends.length];
+        Set<Integer> friends2= new HashSet<>();
+        return ans;
+    }
