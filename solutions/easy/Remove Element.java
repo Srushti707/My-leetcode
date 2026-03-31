5736db8c1c7fcc5871bc18c5edcf94f1bfeec226
Srushti Jamghare <srushtij707@gmail.com>
2026-03-31 22:21:15 +0530

    Add solution for Remove Element

--- solutions/easy/Remove Element.java
+++ solutions/easy/Remove Element.java
@@ -3,20 +3,17 @@
             // Language: Java
             // Link: https://leetcode.com/problems/remove-element/
 
-      }
-}
-        }
-      public void shift(int[] nums, int start )
-      return count;
-            shift(nums,i);
-        {
-        if(nums[i]==val)
-      {
-      for(int i=0; i<nums.length-j; i++)
-        int count=nums.length;
-    public int removeElement(int[] nums, int val) {
-class Solution {
             i--;
-            count--;
-        int j=0;
             j++;
+        }
+      }
+      return count;
+}
+      public void shift(int[] nums, int start )
+      {
+        for(int i=start; i<nums.length-1; i++)
+        {
+            nums[i]=nums[i+1];
+        }
+      }
+    }
