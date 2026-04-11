ae42517c7153a781d69f397a3a5023455779f92a
Srushti Jamghare <srushtij707@gmail.com>
2026-04-11 10:48:32 +0530

    Add solution for Single Number II

---
+++ solutions/medium/Single Number II.java
@@ -0,0 +1,21 @@
+// Title: Single Number II
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/single-number-ii/
+
+        for(int i=0; i<32; i++)
+        {
+            for(int j=0; j<nums.length; j++)
+            {
+                if(((nums[j]>>i)&1)==1)count++;
+        }
+    }
+            int count=0;
+            if(count%3==1) ans=ans|(1<<i);
+        int ans=0;
+        return ans;
+    public int singleNumber(int[] nums) {
+class Solution {
+            
+            }
+}
