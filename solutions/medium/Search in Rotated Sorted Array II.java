cd4cb407c38db0a13f5a8ef3095a4908c92fcda7
Srushti Jamghare <srushtij707@gmail.com>
2026-04-14 22:07:20 +0530

    Add solution for Search in Rotated Sorted Array II

--- solutions/medium/Search in Rotated Sorted Array II.java
+++ solutions/medium/Search in Rotated Sorted Array II.java
@@ -3,21 +3,20 @@
             // Language: Java
             // Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 
-        }
-            if(nums[i]>nums[i+1]) return i;
-        {
-        for(int i=0; i<nums.length-1; i++)
-        if(nums.length==1) return 0;
-    {
-    public int pivot(int[] nums)
-    }
-        else return ans;
-        }
-    }
-     public  boolean BinarySearch(int[]nums, int target , int start , int end)
-     {
-        while(start<=end)
-            return BinarySearch(nums,target,p+1,nums.length-1);
-        {
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int p=pivot(nums);
+        boolean ans=BinarySearch(nums,target,0,p);
         if(ans==false)
-        return nums.length-1;
+        {
+            return BinarySearch(nums,target,p+1,nums.length-1);
+        }
+        else return ans;
+    }
+    public int pivot(int[] nums)
+    {
+        if(nums.length==1) return 0;
+        for(int i=0; i<nums.length-1; i++)
+        {
+            if(nums[i]>nums[i+1]) return i;
+        }
