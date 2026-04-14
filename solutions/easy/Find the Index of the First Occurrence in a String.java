9870442bcc3dbb78f4988e204848492d13a6d4a0
Srushti Jamghare <srushtij707@gmail.com>
2026-04-14 22:01:58 +0530

    Add solution for Find the Index of the First Occurrence in a String

---
+++ solutions/easy/Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,6 @@
+// Title: Find the Index of the First Occurrence in a String
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+c
