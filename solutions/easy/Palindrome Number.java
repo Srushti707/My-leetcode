98599c324f9b8598d6c7ab68f7dddff09eed417f
Srushti Jamghare <srushtij707@gmail.com>
2026-04-28 22:08:15 +0530

    Add solution for Palindrome Number

--- solutions/easy/Palindrome Number.java
+++ solutions/easy/Palindrome Number.java
@@ -3,6 +3,14 @@
             // Language: Java
             // Link: https://leetcode.com/problems/palindrome-number/
 
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0 ))
+        { 
+            return false;
+        }
+        int str= x;
+        int a= 0;
         while(x>a)
         {
             int digit=x%10;
@@ -12,12 +20,3 @@
         }
         if(x==a || x==a/10)
         {
-            return true;
-        int a= 0;
-        int str= x;
-        }
-            return false;
-        { 
-        if(x<0 || (x%10==0 && x!=0 ))
-    public boolean isPalindrome(int x) {
-class Solution {
