e2c940a4f2dfe56a08d63504cec7f7d3bf040add
Srushti Jamghare <srushtij707@gmail.com>
2026-04-29 23:05:45 +0530

    Add solution for Roman to Integer

--- solutions/easy/Roman to Integer.java
+++ solutions/easy/Roman to Integer.java
@@ -3,14 +3,6 @@
             // Language: Java
             // Link: https://leetcode.com/problems/roman-to-integer/
 
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> map= new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
         map.put('D',500);
         map.put('M',1000);
         int sum=0;
@@ -20,3 +12,12 @@ class Solution {
             {
                 sum-=map.get(s.charAt(i));
             }
+            else sum+=map.get(s.charAt(i));
+        }
+        return sum;
+    } 
+        map.put('C',100);
+        map.put('L',50);
+        map.put('X',10);
+        map.put('V',5);
+}
