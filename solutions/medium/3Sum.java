0550660f2fa33ffde289b7fa9d2645b47b1e8408
Srushti Jamghare <srushtij707@gmail.com>
2026-05-07 11:42:05 +0530

    Add solution for 3Sum

---
+++ solutions/medium/3Sum.java
@@ -0,0 +1,23 @@
+// Title: 3Sum
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/3sum/
+
+                    k--;
+                }
+                else if(nums[i]+nums[j]+nums[k]<0)
+                {
+                    j++;
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                {
+                if(nums[i]+nums[j]+nums[k]==0) 
+                    j++;
+            {
+            while(k>j)
+            int k=nums.length-1;
+            int j=i+1;
+            if(i>0 && nums[i-1]==nums[i]) continue;
+        {
+        for(int i=0; i<nums.length-2; i++)
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k&& nums[k]==nums[k+1])k--;
