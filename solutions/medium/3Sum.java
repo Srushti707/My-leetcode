37673983a57233513246b8b3247a212b130700fa
Srushti Jamghare <srushtij707@gmail.com>
2026-05-08 21:40:18 +0530

    Add solution for 3Sum

--- solutions/medium/3Sum.java
+++ solutions/medium/3Sum.java
@@ -3,21 +3,20 @@
             // Language: Java
             // Link: https://leetcode.com/problems/3sum/
 
-                    k--;
-                }
-                else if(nums[i]+nums[j]+nums[k]<0)
-                {
-                    j++;
-                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                {
-                if(nums[i]+nums[j]+nums[k]==0) 
-                    j++;
-            {
-            while(k>j)
-            int k=nums.length-1;
-            int j=i+1;
-            if(i>0 && nums[i-1]==nums[i]) continue;
-        {
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        Arrays.sort(nums);
+        
         for(int i=0; i<nums.length-2; i++)
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k&& nums[k]==nums[k+1])k--;
+        {
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(k>j)
+            {
+                if(nums[i]+nums[j]+nums[k]==0) 
+                {
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
