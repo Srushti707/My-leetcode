fcb9df751aea6c288cb93abd946c82507b71c184
Srushti Jamghare <srushtij707@gmail.com>
2026-06-02 00:23:28 +0530

    Add solution for Minimum Cost of Buying Candies With Discount

---
+++ solutions/easy/Minimum Cost of Buying Candies With Discount.java
@@ -0,0 +1,23 @@
+// Title: Minimum Cost of Buying Candies With Discount
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/
+
+       for(int i=cost.length-1; i>=0; i--)
+       {
+        if(c%3==0)
+       } 
+       {
+        for(int num:cost) tcost+=num;
+       }
+        return tcost;
+       int c=1;
+        tcost+=cost[i];
+        {
+            continue;
+        } 
+            c++;
+       return tcost;
+    }
+        c++;
+}
