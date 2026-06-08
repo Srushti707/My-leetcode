2164a25d4ee9a16f5b5664416575c55693c34e03
Srushti Jamghare <srushtij707@gmail.com>
2026-06-08 18:36:54 +0530

    Add solution for Partition Array According to Given Pivot

---
+++ solutions/medium/Partition Array According to Given Pivot.java
@@ -0,0 +1,26 @@
+// Title: Partition Array According to Given Pivot
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/partition-array-according-to-given-pivot/
+
+     for(int i=0; i<arr1.size(); i++)
+     int index=0;
+     }
+        }
+            arr3.add(num);
+        {
+        else
+     {
+        ans[index]=arr1.get(i);
+        index+=1;
+     }
+     for(int j=0; j<arr2.size(); j++)
+     {
+        ans[index]=arr2.get(j);
+        index+=1;
+     }
+     for(int k=0; k<arr3.size(); k++)
+     {
+        ans[index]=arr3.get(k);
+        index+=1;
+     }
