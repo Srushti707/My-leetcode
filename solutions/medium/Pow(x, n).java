e686255cdcd02f84468b8be7adeab8854419b00c
Srushti Jamghare <srushtij707@gmail.com>
2026-06-11 22:38:30 +0530

    Add solution for Pow(x, n)

---
+++ solutions/medium/Pow(x, n).java
@@ -0,0 +1,25 @@
+// Title: Pow(x, n)
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/pow(x,-n)/
+
+        }
+            x*=x;
+            N=N>>1;
+        double ans=1;
+        while(N>0)
+        {
+            if((N & 1)==1)
+            {
+                ans=ans*x;
+            }
+            System.out.println(ans);
+        }
+            N=-N;
+            x=1/x;
+        {
+        if(N<0)
+        long N=n;
+        //convert negative n to positive
+    public double myPow(double x, int n) {
+class Solution {
