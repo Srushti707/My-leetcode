f1ceb2cc7f5f75503ef896aafd158b9d24d2f6c9
Srushti Jamghare <srushtij707@gmail.com>
2026-06-21 21:20:54 +0530

    Add solution for Maximum Ice Cream Bars

---
+++ solutions/medium/Maximum Ice Cream Bars.java
@@ -0,0 +1,21 @@
+// Title: Maximum Ice Cream Bars
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-ice-cream-bars/
+
+       Arrays.sort(costs);
+       int bars=0;
+       for(int cost: costs)
+       {
+        if(coins>=cost)
+       } 
+            bars++;
+        }
+    }
+        {
+            coins-=cost;
+       return bars;
+        else return bars;
+}
+    public int maxIceCream(int[] costs, int coins) {
+class Solution {
