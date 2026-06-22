59d52893c884165c3a311ede7abf89d57d10b8d6
Srushti Jamghare <srushtij707@gmail.com>
2026-06-22 23:50:43 +0530

    Add solution for Maximum Number of Balloons

---
+++ solutions/easy/Maximum Number of Balloons.java
@@ -0,0 +1,25 @@
+// Title: Maximum Number of Balloons
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-number-of-balloons/
+
+            if(entry.getKey()=='l' || entry.getKey()=='o') 
+            {
+                balloons[i]=entry.getValue()/2;
+                i++;
+            }
+                balloons[i]=entry.getValue();
+                if(balloons[i]<min) min=balloons[i];
+            int i=0;
+        {
+        for(Map.Entry<Character,Integer> entry: map.entrySet())
+        int min=Integer.MAX_VALUE;
+        }
+                if(balloons[i]<min) min=balloons[i];
+                i++;
+
+        }
+        return min;
+            }
+            else
+            {
