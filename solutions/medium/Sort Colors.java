fb5f16f82706d1b74b7cef48c32c7daa09d1feff
Srushti Jamghare <srushtij707@gmail.com>
2026-06-23 23:41:01 +0530

    Add solution for Sort Colors

--- solutions/medium/Sort Colors.java
+++ solutions/medium/Sort Colors.java
@@ -3,19 +3,19 @@
             // Language: Java
             // Link: https://leetcode.com/problems/sort-colors/
 
+class Solution {
     public void sortColors(int[] nums) {
         for(int i=0;i<nums.length-1; i++)
         {
+            for(int j=nums.length-1; j>i; j--)
             {
                 if(nums[j]<nums[i])
                 {
                     int temp=nums[j];
-                }
                     nums[j]=nums[i];
                     nums[i]=temp;
+                }
             }
         }
-            for(int j=nums.length-1; j>i; j--)
-class Solution {
     }
 }
