c4f80c0347687ab2030e7ca64d246f363e4f5767
Srushti Jamghare <srushtij707@gmail.com>
2026-06-25 23:42:14 +0530

    Add solution for First Missing Positive

--- solutions/hard/First Missing Positive.java
+++ solutions/hard/First Missing Positive.java
@@ -10,7 +10,7 @@ class Solution {
         {
             int correctIndex=nums[i]-1;
             if(nums[i]>0 && nums[i]<nums.length+1 && nums[i]!= nums[correctIndex]) swap(nums,i,
-correctIndex);
+            correctIndex);
             else i++;
         }
         for(int j=0; j<nums.length;j++)
@@ -20,3 +20,6 @@ correctIndex);
         return nums.length+1;
     }
     static void swap(int[] arr, int first , int second)
+    {
+        int temp=arr[first];
+        arr[first]=arr[second];
