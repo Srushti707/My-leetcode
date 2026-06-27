f60920cf8f9a84958841fe66bd3946dc13b0d8cb
Srushti Jamghare <srushtij707@gmail.com>
2026-06-27 22:09:56 +0530

    Add solution for Search Insert Position

--- solutions/easy/Search Insert Position.java
+++ solutions/easy/Search Insert Position.java
@@ -3,23 +3,23 @@
             // Language: Java
             // Link: https://leetcode.com/problems/search-insert-position/
 
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        return binarySearch(nums ,target);
-        
-    }
-
-    public int binarySearch(int[] nums , int target)
-    {
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end)
-        {
-            int mid= start+(end-start)/2;
-            if(nums[mid]==target)
             {
                 return mid;
             }
             else if(nums[mid]>target)
             {
                 end=mid-1;
+            if(nums[mid]==target)
+            int mid= start+(end-start)/2;
+        while(start<=end)
+        {
+    {
+        int start=0;
+        int end=nums.length-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+
+        }
