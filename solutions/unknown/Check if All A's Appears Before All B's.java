84ad790080d9f297731e1b520ffc9a528d589c1d
Srushti Jamghare <srushtij707@gmail.com>
2026-07-01 23:41:03 +0530

    Add solution for Check if All A's Appears Before All B's

---
+++ solutions/unknown/Check if All A's Appears Before All B's.java
@@ -0,0 +1,5 @@
+// Title: Check if All A's Appears Before All B's
+            // Difficulty: Unknown
+            // Language: Java
+            // Link: https://leetcode.com/problems/check-if-all-a's-appears-before-all-b's/
+
