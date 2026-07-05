1243de4b2403d7d0cae7cc77c51e0bde002131e8
Srushti Jamghare <srushtij707@gmail.com>
2026-07-05 23:27:26 +0530

    Add solution for First Missing Positive

--- solutions/hard/First Missing Positive.java
+++ solutions/hard/First Missing Positive.java
@@ -3,13 +3,6 @@
             // Language: Java
             // Link: https://leetcode.com/problems/first-missing-positive/
 
-class Solution {
-    public int firstMissingPositive(int[] nums) {
-        int i=0;
-        while(i<nums.length)
-        {
-            int correctIndex=nums[i]-1;
-            if(nums[i]>0 && nums[i]<nums.length+1 && nums[i]!= nums[correctIndex]) swap(nums,i,
             correctIndex);
             else i++;
         }
@@ -23,3 +16,6 @@ class Solution {
     {
         int temp=arr[first];
         arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
