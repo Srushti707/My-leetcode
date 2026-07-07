0e186cbd568eae19f58946166821475f19349f4f
Srushti Jamghare <srushtij707@gmail.com>
2026-07-08 00:31:57 +0530

    Add solution for Subsets

--- solutions/medium/Subsets.java
+++ solutions/medium/Subsets.java
@@ -3,6 +3,11 @@
             // Language: Java
             // Link: https://leetcode.com/problems/subsets/
 
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        subset(nums,new ArrayList<>(),ans,0);
+        return ans;
     }
 
     static void subset(int[] nums,List<Integer>list, List<List<Integer>> ans, int i )
@@ -10,16 +15,11 @@
         if(i>=nums.length)
         {
             ans.add(new ArrayList<>(list));
-        }
             return;
+        }
 
         //pick
         list.add(nums[i]);
         subset(nums,list,ans,i+1);
         list.remove(list.size()-1);
         //not pick
-        return ans;
-        subset(nums,new ArrayList<>(),ans,0);
-        List<List<Integer>> ans= new ArrayList<>();
-        subset(nums,list,ans,i+1);
-    }
