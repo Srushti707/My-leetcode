03240a57a2120acc0b7c3d59dedeea6bdf76cf24
Srushti Jamghare <srushtij707@gmail.com>
2026-07-10 23:52:42 +0530

    Add solution for Build Array from Permutation

---
+++ solutions/easy/Build Array from Permutation.java
@@ -0,0 +1,15 @@
+// Title: Build Array from Permutation
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans =new int[nums.length];
+        for(int i=0; i<nums.length; i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
