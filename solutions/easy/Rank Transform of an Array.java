3f021c6aaa2c6f86b0d4ebb2d863e7f6e79859fc
Srushti Jamghare <srushtij707@gmail.com>
2026-07-13 00:02:41 +0530

    Add solution for Rank Transform of an Array

---
+++ solutions/easy/Rank Transform of an Array.java
@@ -0,0 +1,25 @@
+// Title: Rank Transform of an Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/rank-transform-of-an-array/
+
+        Map <Integer,Integer> map= new HashMap<>();
+        for(int i=0; i<arr.length; i++)
+        {
+        }
+        for(int j=0; j<arr.length; j++)
+        {
+            ans[j]=map.get(arr[j]);
+        }
+        int[] ans=new int[arr.length];
+        return ans;
+            if(!map.containsKey(sorted[i]))
+            {
+            }
+        Arrays.sort(sorted);
+        int[] sorted= Arrays.copyOf(arr,arr.length);
+    public int[] arrayRankTransform(int[] arr) {
+class Solution {
+        int count=0;
+            map.put(sorted[i],++count);
+    }
