e1307f9217863614cbd1efdf70ca1fcd464beeb6
Srushti Jamghare <srushtij707@gmail.com>
2026-07-14 23:57:26 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.java
+++ solutions/easy/Two Sum.java
@@ -3,4 +3,19 @@
             // Language: Java
             // Link: https://leetcode.com/problems/two-sum/
 
-c
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; 
+    }
+}
