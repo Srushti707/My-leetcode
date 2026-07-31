44d2d3e75c49b7189cd3a615c4f08d95253f984a
Srushti Jamghare <srushtij707@gmail.com>
2026-08-01 00:34:54 +0530

    Add solution for Maximum Product of Two Digits

--- solutions/easy/Maximum Product of Two Digits.java
+++ solutions/easy/Maximum Product of Two Digits.java
@@ -6,19 +6,19 @@
 class Solution {
     public int maxProduct(int n) {
         int max1=0, max2=0;
+        
         while(n>0)
         {
             int num=n%10;
             if(num>max1)
-        }
-            else if(num>max2) max2=num;
-        return max1*max2;
-            n=n/10;
-
-        
             {
-                max1=num;
-    }
-            } 
                 max2=max1;
+                max1=num;
+            } 
+            else if(num>max2) max2=num;
+            n=n/10;
+        }
+
+        return max1*max2;
+    }
 }
